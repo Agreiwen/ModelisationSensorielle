639edc3ab1df5f245fd4148dc667c7942609ceed
Adrien J <ajeanne25@gmail.com>
2017-02-22 13:59:00 +0100

    lecture hmm params

--- td1/Hmm.java
+++ td1/Hmm.java
@@ -6,17 +6,37 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class Hmm {
 	
+	protected double psub;
+	protected double pins;
+	protected double pomi;
+
+	protected HashMap<String, HashMap<String,Double>> matriceTransition;
+	
 	public Hmm(String fichierTest) throws IOException{
-		//motsTest = new HashMap<>();
-		//motsLexique = new HashMap<>();
+
 		lectureFichierTest(fichierTest);
-		//lectureLexique(lexique);
 	}
 
+	private double getCsub(String phonemetest, String phonemeref) {
+		return -Math.log(psub)-Math.log(matriceTransition.get(phonemetest).get(phonemeref));
+		
+	}
+	
+	private double getCins(String phoneme1test) {
+		return -Math.log(pins)-Math.log(matriceTransition.get(phoneme1test).get("<ins>"));
+		
+	}
+
+	private double getComi() {
+		return -Math.log(pomi);
+		
+	}
+	
 	private void lectureFichierTest(String fichierTest) throws IOException {
 		String st = "";
 		String partie1 = "";
@@ -28,7 +48,7 @@ public class Hmm {
         @SuppressWarnings("unused")
 		Pattern pattern = Pattern.compile(tab);
         br = new BufferedReader(new FileReader(fichierTest));
-        System.out.print("Lecture du fichier test... ");
+        System.out.println("Lecture du fichier test... ");
         
         String nomproba = br.readLine();
         String valeurproba = br.readLine();
@@ -36,17 +56,32 @@ public class Hmm {
         String psub = separated[0];
         String pins = separated[1];
         String pomi = separated[2];
-        
+        this.psub = Double.parseDouble(psub);
+        this.pins = Double.parseDouble(pins);
+        this.pomi = Double.parseDouble(pomi);
         String commentaire = br.readLine();
+        String coltableau = br.readLine();
+        String[] separated2 = coltableau.split(tab);
+
+        matriceTransition = new HashMap<>();
+        
+        for (int i = 1; i < separated2.length; i++) {
+        	HashMap<String,Double> matricetmp = new HashMap<>();
+        	for (int j = 1; j < separated2.length; j++) {
+        		matricetmp.put(separated2[j], 0.);
+        	}
+        	matriceTransition.put(separated2[i], matricetmp);
+		}
+
         while ((st = br.readLine()) != null) {
         	separated = st.split(tab);
-        	for (int i = 0; i < separated.length; i++) {
-        		System.out.println(separated[i]+" endmot ");
+        	for (int i = 1; i < separated.length; i++) {
+        		
+        		    matriceTransition.get(separated2[i]).put(separated[0],Double.parseDouble(separated[i]));
+        		    System.out.println("Abscisse :"+separated2[i]+" Ordonnee :"+separated[0]+"  valeur :"+separated[i]);
+   				
 			}
-        	
-        	System.out.println(" endligne ");
-        	
-        }
+    	}
         System.out.println("Termine.");
         br.close();
 		

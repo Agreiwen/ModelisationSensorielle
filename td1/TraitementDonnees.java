d72dab37ee37e68f7d972853eb7735bd9db0d08e
Aurélien <aurel.andre@yahoo.fr>
2017-02-22 13:59:01 +0100

    correction

--- td1/TraitementDonnees.java
+++ td1/TraitementDonnees.java
@@ -16,9 +16,6 @@ public class TraitementDonnees {
 	}
 
 	public int distanceLevenshtein(String mot1, String f1, String mot2, String f2) {
-
-		//System.out.print(mot1 + " [" + f1 + "]");
-		//System.out.print(" => " + mot2 + " [" + f2 + "] ");
 		affichageDistLeven = "";
 		affichageDistLeven += mot1 + " [" + f1 + "]";
 		affichageDistLeven += " => " + mot2 + " [" + f2 + "] ";
@@ -26,16 +23,13 @@ public class TraitementDonnees {
 		String[] t1 = f1.split(" ");
 		String[] t2 = f2.split(" ");
 		return levenshteinCalcul(t1, t2);
-
 	}
 
 	public int levenshteinCalcul(String[] f1, String[] f2) {
 		int size1 = f1.length;
 		int size2 = f2.length;
-	
 
 		int[][] tmp = new int[size1 + 1][size2 + 1];
-		String[][] action = new String[size1 + 1][size2 + 1];
 
 		for (int i = 0; i <= size1; i++) {
 			tmp[i][0] = i;
@@ -50,29 +44,17 @@ public class TraitementDonnees {
 				int omi = tmp[i - 1][j] + 1;
 				int inser = tmp[i][j - 1] + 1;
 				int sub = tmp[i - 1][j - 1] + m;
-
-				tmp[i][j] = Math.min(Math.min(tmp[i - 1][j] + 1, tmp[i][j - 1] + 1), tmp[i - 1][j - 1] + m);
-
-				// faux de faire ça, on l'utilise plus d'ailleurs
-				if (tmp[i][j] == 0)
-					action[i][j] = "correct";
-				else if (tmp[i][j] == omi)
-					action[i][j] = "omi";
-				else if (tmp[i][j] == inser)
-					action[i][j] = "inser";
-				else if (tmp[i][j] == sub)
-					action[i][j] = "sub";
+				tmp[i][j] = Math.min(Math.min(omi, inser), sub);
 			}
 		}
 
-		if (tmp[size1][size2] > 0)
+		if (tmp[size1][size2] > 0){
 			affichageDistLeven += "Erreur " + tmp[size1][size2] + " <=> ";
-			//System.out.print("Erreur " + tmp[size1][size2] + " <=> ");
-		else
+		}
+		else{
 			affichageDistLeven += "Correct " + tmp[size1][size2] + " <=> ";
-			//System.out.print("Correct " + tmp[size1][size2] + " <=> ");
+		}
 
-		// ALGO TROIS QUI MARCHE
 		int j = 0;
 		int i = 0;
 		boolean fini = false;
@@ -96,42 +78,29 @@ public class TraitementDonnees {
 				droite = 500;
 
 			int min = Math.min(haut, Math.min(diago, droite));
-			// System.out.print("| diago:"+diago+" haut:"+haut+"
-			// droite:"+droite+" ");
 			if (min == diago) {
 				j++;
 				i++;
 				affichageDistLeven += " s(" + f1[i - 1] + "=>" + f2[j - 1] + ")";
-				//System.out.print(" s(" + f1[i - 1] + "=>" + f2[j - 1] + ")");
-
 			} else if (min == haut) {
 				j++;
 				affichageDistLeven += " i(=>" + f2[j - 1] + ")";
-				//System.out.print(" i(=>" + f2[j - 1] + ")");
-
 			} else if (min == droite) {
 				i++;
 				affichageDistLeven += " o(=>" + f1[i - 1] + ")";
-				//System.out.print(" o(=>" + f1[i - 1] + ")");
-
 			} else
 				System.out.println("erreur");
-
 		}
 		affichageDistLeven += "\n";
-		//System.out.println();
-
-		// System.out.println("------------");
-
 		return tmp[size1][size2];
 	}
 
+	@SuppressWarnings("unused")
 	public void reconnaissanceLevenshtein() {
-		StringBuilder sb;
 		int distanceLevenshtein;
 		int minDistanceLevenshtein;
 		String motReconnu;
-		String phonemesMotReconnu;
+		String phonemesMotReconnu = "";
 		int nombreErreur = 0;
 		int nombreCorrect = 0;
 		System.out.println("pour chaque action : s = substitution o = omission i = insertion \n");
@@ -158,14 +127,11 @@ public class TraitementDonnees {
 				}
 				System.out.println(affichageMotReconnu);
 				if (!motReconnu.equals(motTest)) {
-					//sb.append("Erreur");
 					nombreErreur++;
 				} else {
-					//sb.append("Correct");
 					nombreCorrect++;
 				}
 			}
-			
 		}
 		System.out.println();
 		System.out.println("Nombre d'erreur = " + nombreErreur + " et nombre correct = " + nombreCorrect);
